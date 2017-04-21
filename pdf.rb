eab5eff068d222aaf73d2171aaa5105f5b653172
stephan <stephan@hirsch-online.com>
2017-04-21 17:32:38 +0200

    Uncommenting

--- pdf.rb
+++ pdf.rb
@@ -152,7 +152,7 @@ cmd1 = 'sed -i -E "s/\|([a-zA-Z0-9#])/\$\^\1\$/g" example.tex'
 cmd2 = 'pdflatex -interaction nonstopmode --enable-write18 -shell-escape -output-directory="." example.tex > /dev/null'
 cmd3 = 'rubber --pdf example'
 system( cmd1 )
-#system( cmd2 )
+system( cmd2 )
 system( cmd3 )
 if ofile != "/tmp/example.pdf"
   cmd4 = "cp example.pdf #{prog_path}/#{ofile}"
